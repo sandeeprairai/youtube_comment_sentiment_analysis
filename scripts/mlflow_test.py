400d59821d51a97c888f64c094265ff495da33c6
sandeeprairai <sandeeprairai339@gmail.com>
2024-12-18 14:45:03 +0530

    Added mlflo workflow

---
+++ scripts/mlflow_test.py
@@ -0,0 +1,18 @@
+import mlflow 
+import random 
+
+
+# Set the Mlflow tracking URI
+mlflow.set_tracking_uri("http://ec2-13-58-171-127.us-east-2.compute.amazonaws.com:5000/")
+
+
+# Start an Mlflow run
+with mlflow.start_run():
+    mlflow.log_param("param1",random.randint(1,100))
+    mlflow.log_param("param2",random.random())
+
+    # Log some random metrics
+    mlflow.log_metric("metric1",random.random())
+    mlflow.log_metric("metric2",random.uniform(0.5,1.5))
+
+    print("Logged random parametrs and metrics")
