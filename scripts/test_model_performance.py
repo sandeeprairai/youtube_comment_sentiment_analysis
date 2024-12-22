a7770efa529333db6fc031fe5fe96fdc925b1ef6
sandeeprairai <sandeeprairai339@gmail.com>
2024-12-22 15:13:41 +0530

    add dockerfile

--- scripts/test_model_performance.py
+++ scripts/test_model_performance.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import mlflow
 
 # Set your remote tracking URI
-mlflow.set_tracking_uri("http://ec2-3-143-213-62.us-east-2.compute.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-18-218-93-205.us-east-2.compute.amazonaws.com:5000/")
 
 @pytest.mark.parametrize("model_name, stage, holdout_data_path, vectorizer_path", [
     ("yt_chrome_plugin_model", "staging", "data/interim/test_processed.csv", "tfidf_vectorizer.pkl"),  # Replace with your actual paths
